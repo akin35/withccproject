7d383384bb0a9f91706b4150e86863f8d941d5d0
Akin Kuyga <akinkuyga35@gmail.com>
2023-01-31 00:44:48 -0500

    initial

---
+++ main.tf
@@ -0,0 +1,18 @@
+resource "azurerm_resource_group" "rg1" {
+  name     = var.rg_name
+  location = var.location
+}
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "rg_name" {
+  default = "rg1"
+}
+
+variable "location" {
+  default = "East_US"
+}
+
+variable "virtual_network_name" {
+  default = "vnet"
+}
+
+variable "rg_name" {
+  default = "rg1"
+}
+
+variable "rg_name" {
+  default = "rg1"
+}
